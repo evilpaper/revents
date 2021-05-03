6e5bb1a2a289687c1ec5d81266a7872394c5d5f5
evilpaper <pellburt@gmail.com>
2021-05-03 07:19:37 +0200

    Clean up sandbox

--- src/features/sandbox/Sandbox.jsx
+++ src/features/sandbox/Sandbox.jsx
@@ -2,43 +2,34 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Button } from "semantic-ui-react";
 import { openModal } from "../../app/common/modal/modalReducer";
-import { increment, decrement, add, remove } from "./testReducer";
+import { increment, decrement } from "./testReducer";
 
 export default function Sandbox() {
   const dispatch = useDispatch();
   const count = useSelector((state) => state.test.count);
-  const todos = useSelector((state) => state.test.todos);
-  const data = useSelector((state) => state.test.count);
 
   return (
     <>
       <h1>This is the Sandbox Component for playing around with stuff</h1>
       <h3>Count = {count}</h3>
       <Button
+        onClick={() => dispatch(decrement(1))}
+        content="Decrement"
+        color="red"
+      />
+      <Button
         onClick={() => dispatch(increment(1))}
         content="Increment"
         color="green"
       />
       <Button
-        onClick={() => dispatch(decrement(1))}
-        content="Decrement"
-        color="red"
-      />
-      <h3>Todos</h3>
-      <Button content="Add" color="blue" onClick={() => dispatch(add())} />
-      <Button
         content="Open Modal"
         basic
         color="teal"
         onClick={() =>
-          dispatch(openModal({ modalType: "TestModal", modalProps: { data } }))
+          dispatch(openModal({ modalType: "TestModal", modalProps: { count } }))
         }
       />
-      <ul>
-        {todos.map((todo) => {
-          return <li>{todo}</li>;
-        })}
-      </ul>
     </>
   );
 }
--- src/features/sandbox/TestModal.jsx
+++ src/features/sandbox/TestModal.jsx
@@ -1,9 +1,9 @@
 import ModalWrapper from "../../app/common/modal/ModalWrapper";
 
-export default function TestModal({ data }) {
+export default function TestModal({ count }) {
   return (
     <ModalWrapper size="mini" header="Test Modal">
-      <div>The data is: {data}</div>
+      <div>The data is: {count}</div>
     </ModalWrapper>
   );
 }
