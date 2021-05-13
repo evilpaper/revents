44377ffa59c80d50ed7eba3bba09960d36c9f708
evilpaper <pellburt@gmail.com>
2021-05-13 15:37:38 +0200

    Isolate sandbox button loading indicators

--- src/features/sandbox/Sandbox.jsx
+++ src/features/sandbox/Sandbox.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Button } from "semantic-ui-react";
 import { openModal } from "../../app/common/modal/modalReducer";
@@ -6,6 +6,7 @@ import { increment, decrement } from "./testReducer";
 
 export default function Sandbox() {
   const dispatch = useDispatch();
+  const [target, setTarget] = useState(null);
   const count = useSelector((state) => state.test.count);
   const { loading } = useSelector((state) => state.async);
 
@@ -14,17 +15,26 @@ export default function Sandbox() {
       <h1>This is the Sandbox Component for playing around with stuff</h1>
       <h3>Count = {count}</h3>
       <Button
-        loading={loading}
-        onClick={() => dispatch(decrement(1))}
-        content="Decrement"
-        color="red"
-      />
-      <Button
-        loading={loading}
-        onClick={() => dispatch(increment(1))}
+        name="increment"
+        loading={loading && target === "increment"}
+        onClick={(e) => {
+          dispatch(increment(1));
+          setTarget(e.target.name);
+        }}
         content="Increment"
         color="green"
       />
+      <Button
+        name="decrement"
+        loading={loading && target === "decrement"}
+        onClick={(e) => {
+          dispatch(decrement(1));
+          setTarget(e.target.name);
+        }}
+        content="Decrement"
+        color="red"
+      />
+
       <Button
         content="Open Modal"
         basic
