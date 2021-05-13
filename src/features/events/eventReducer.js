f8f4915db4ac8aa14bd366ed80bdda0a4cb42665
evilpaper <pellburt@gmail.com>
2021-05-13 17:16:49 +0200

    Add mocked  asynchronous API call

--- src/features/events/eventReducer.js
+++ src/features/events/eventReducer.js
@@ -1,8 +1,13 @@
 import { sampleData } from "../../app/api/sampleData";
-import { CREATE_EVENT, DELETE_EVENT, UPDATE_EVENT } from "./eventConstants";
+import {
+  CREATE_EVENT,
+  DELETE_EVENT,
+  FETCH_EVENT,
+  UPDATE_EVENT,
+} from "./eventConstants";
 
 const initialState = {
-  events: sampleData,
+  events: [],
 };
 
 export default function eventReducer(state = initialState, { type, payload }) {
@@ -23,7 +28,13 @@ export default function eventReducer(state = initialState, { type, payload }) {
     case DELETE_EVENT:
       return {
         ...state,
-        events: [...state.events.filter((evt) => evt.id !== payload)],
+        events: [...payload],
+      };
+
+    case FETCH_EVENT:
+      return {
+        ...state,
+        events: [...payload],
       };
     default:
       return state;
